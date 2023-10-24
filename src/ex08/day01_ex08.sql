f10c859ffa8489c502ea5e84d04ffffba992ace9
bonnyped <bonnyped@student.21-school.ru>
2023-10-25 01:11:56 +0600

    minor changes

---
+++ src/ex08/day01_ex08.sql
@@ -0,0 +1,11 @@
+WITH tmp AS (SELECT p.id AS person_id,
+                    p.name,
+                    p.age
+             FROM person p
+            )
+SELECT
+      order_date,
+       concat(tmp.name, ' (age:', tmp.age, ')') AS person_information
+FROM tmp
+NATURAL JOIN person_order po
+ORDER BY order_date, person_information
